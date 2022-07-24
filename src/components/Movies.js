4a612ae8a4ea2d988b6922063391dfa0763e1543
HASSAN1A <jumahassandev@gmail.com>
2022-07-25 01:08:52 +0300

    add movies component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,33 @@
 import React from "react";
 import { movies } from "../data";
+const movieItems = movies.map((movie) =>{
+  return(
+    <div key={movie.title}>
+    <h1>Name: {movie.title}</h1>
+     <h3>Time: {movie.time}</h3>
+     <h3>Genres</h3>
+     <ul>
+       {movie.genres.map((gen) =>(
+         <li
+         key={gen}>
+           {gen}
+         </li>
+       ))}
+     </ul>
+    
+
+  </div>
+  );
+  
+});
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieItems}
+    </div>
+  );
 }
 
 export default Movies;
