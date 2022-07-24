8547eee30429f65a963ea7cacf6bd39edabf7e86
HASSAN1A <jumahassandev@gmail.com>
2022-07-25 01:08:21 +0300

    Add directors component

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,30 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorList = directors.map((director) =>{
+    return(
+      <div 
+      key={director.name}
+      >
+        <h1>Name: {director.name}</h1>
+        <h3>Movies</h3>
+        <ul>
+          {director.movies.map((d) =>(
+            <li 
+            key={d}>
+              {d}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  });
+  return(
+    <div>
+      <h1>Directors Page</h1>
+      {directorList}
+    </div>
+  );
 }
 
 export default Directors;
